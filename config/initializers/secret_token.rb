371bbc5e1a6f1d2892691f4ed639f24e0f7fad51
Colin Manuel <colin.manuel@gmail.com>
2013-06-11 18:31:24 -0500

    Generate initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puppal::Application.config.secret_token = '7d2a26d69a754003053f243d8c7a12fac4de882d37645937dea491d45be85a11a33cd7afc4a839edd81abbb547aac599d3cbb4e70450b28350d5f9c8342cce14'
