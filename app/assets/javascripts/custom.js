d76abee66c95da53b3921d3a1b0dbbe27ca558be
Colin Manuel <colin.manuel@gmail.com>
2013-06-23 17:41:13 -0500

    make panel open accordions when on home page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,49 @@
 
 $( document ).ready(function() {
+ // make nav panel toggle accordions
+    // name
+    $('.panel-by-name').on('click', function (e){
+    	$( "#nav-panel" ).panel( "close" );
+	    $('#by-name').trigger('expand');
+	});
+
+	// location
+    $('.panel-by-location').on('click', function (e){
+    	$( "#nav-panel" ).panel( "close" );
+	    $('#by-location').trigger('expand');
+	});
+
+	// priority
+    $('.panel-by-priority').on('click', function (e){
+    	$( "#nav-panel" ).panel( "close" );
+	    $('#by-priority').trigger('expand');
+	});
+
+	// bios
+    $('.panel-need-bios').on('click', function (e){
+    	$( "#nav-panel" ).panel( "close" );
+	    $('#by-need').trigger('expand');
+	    $('#need-bios').trigger('expand');
+	});
+
+	// photos
+    $('.panel-need-photos').on('click', function (e){
+    	$( "#nav-panel" ).panel( "close" );
+	    $('#by-need').trigger('expand');
+	    $('#need-photos').trigger('expand');
+	});
+
+	// videos
+    $('.panel-need-videos').on('click', function (e){
+    	$( "#nav-panel" ).panel( "close" );
+	    $('#by-need').trigger('expand');
+	    $('#need-videos').trigger('expand');
+
+	});
+
+
+
+
 	// toggle photo upload button
 	$('input.photo-complete').on('click', function (e){
 		$('#upload-photo').hide();
