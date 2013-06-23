b56b0f6326bad6409750481324331cf669460a64
Colin Manuel <colin.manuel@gmail.com>
2013-06-23 18:43:16 -0500

    create readme file for github

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,39 +2,39 @@
 $( document ).ready(function() {
  // make nav panel toggle accordions
     // name
-    $('.panel-by-name').on('click', function (e){
+    $('.panel-by-name').on('click', function (){
     	$( "#nav-panel" ).panel( "close" );
 	    $('#by-name').trigger('expand');
 	});
 
 	// location
-    $('.panel-by-location').on('click', function (e){
+    $('.panel-by-location').on('click', function (){
     	$( "#nav-panel" ).panel( "close" );
 	    $('#by-location').trigger('expand');
 	});
 
 	// priority
-    $('.panel-by-priority').on('click', function (e){
+    $('.panel-by-priority').on('click', function (){
     	$( "#nav-panel" ).panel( "close" );
 	    $('#by-priority').trigger('expand');
 	});
 
 	// bios
-    $('.panel-need-bios').on('click', function (e){
+    $('.panel-need-bios').on('click', function (){
     	$( "#nav-panel" ).panel( "close" );
 	    $('#by-need').trigger('expand');
 	    $('#need-bios').trigger('expand');
 	});
 
 	// photos
-    $('.panel-need-photos').on('click', function (e){
+    $('.panel-need-photos').on('click', function (){
     	$( "#nav-panel" ).panel( "close" );
 	    $('#by-need').trigger('expand');
 	    $('#need-photos').trigger('expand');
 	});
 
 	// videos
-    $('.panel-need-videos').on('click', function (e){
+    $('.panel-need-videos').on('click', function (){
     	$( "#nav-panel" ).panel( "close" );
 	    $('#by-need').trigger('expand');
 	    $('#need-videos').trigger('expand');
@@ -45,18 +45,18 @@ $( document ).ready(function() {
 
 
 	// toggle photo upload button
-	$('input.photo-complete').on('click', function (e){
+	$('input.photo-complete').on('click', function (){
 		$('#upload-photo').hide();
 	});
-	$('input.photo-outdated, input.photo-incomplete').on('click', function (e){
+	$('input.photo-outdated, input.photo-incomplete').on('click', function (){
 		$('#upload-photo').show();
 	});
 
 	// toggle video upload button
-	$('input.video-complete').on('click', function (e){
+	$('input.video-complete').on('click', function (){
 		$('#upload-video').hide();
 	});
-	$('input.video-outdated, input.video-incomplete').on('click', function (e){
+	$('input.video-outdated, input.video-incomplete').on('click', function (){
 		$('#upload-video').show();
 	});
 
---
+++ readme.md
@@ -0,0 +1,35 @@
+PupPal Prototype
+===============================
+
+this is a test. 
+
+A prototype demo is previewable on Heroku at http://protected-savannah-4558.herokuapp.com/dogs. 
+
+## Goal:
+PupPal is designed to lower the barrier to publication for volunteers. Using their smartphones as both navigational
+
+## Features:
+
+- Find dogs by 
+- feature two
+
+## Dependencies:
+- Ruby on Rails
+- jQuery Mobile
+
+## Caveats:
+- PupPal has only been tested on an iPhone 5
+- Speed and security improvements are still in the future 
+
+## Improvements
+- Administrative login
+- SmugMug photo and video upload and preview
+- Backbone.js implementation
+- Google Map directions to sites hosting dogs
+- Administrative back end for desktop management
+- Database design improvements
+- Ajax form loading and refresh
+- Possible app migration via PhoneGap
+
+## License
+This code is still in its infancy. In the coming months, a more refined and replicable version will be released to the Creative Commons.
